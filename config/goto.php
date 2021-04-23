000c15b12671338a20fbb614c55fbfae34e0f1f3
Seth <seth@netready.biz>
2021-04-23 16:29:10 +0100

    changes

--- config/goto.php
+++ config/goto.php
@@ -5,6 +5,7 @@ return [
     'client_secret' => env('GOTO_CONSUMER_SECRET', 'someConsumerSecret'),
     'direct_username' => env('GOTO_DIRECT_USERNAME', 'someUsername'),
     'direct_password' => env('GOTO_DIRECT_PASSWORD', 'somePassword'),
+    'callback_url' => env('GOTO_DIRECT_PASSWORD', 'someCallback'),
 
     'subject_suffix' => env('GOTO_SUBJECT_SUFFIX', null),
     'webinar_link' => env('GOTO_WEBINAR_LINK', 'https://global.gotowebinar.com/manageWebinar.tmpl?webinar=%s'),
