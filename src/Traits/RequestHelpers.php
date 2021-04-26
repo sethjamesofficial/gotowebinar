2138fea95559c8b31681ec14bc9e95ad2421f446
Seth <seth@netready.biz>
2021-04-26 12:12:00 +0100

    changes

--- src/Traits/RequestHelpers.php
+++ src/Traits/RequestHelpers.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Slakbal\Gotowebinar\Traits;
+use Illuminate\Support\Arr;
 
 trait RequestHelpers
 {
@@ -37,7 +38,7 @@ trait RequestHelpers
         //list of variables to be filtered out
         $ignore = $this->getPayloadExclusions();
 
-        return array_where(get_object_vars($this), function ($value, $key) use ($ignore) {
+        return Arr:where(get_object_vars($this), function ($value, $key) use ($ignore) {
             if (! in_array($key, $ignore)) {
                 return ! empty($value);
             }
